85497536d7e427b440586c523ff7e8ace896c3cc
efkag <kagioulis.efstathios@gmail.com>
2019-11-13 15:59:00 +0000

    create class to perform becnhmarking

---
+++ main.py
@@ -0,0 +1,11 @@
+import alg_bench
+
+def main():
+    window_range = list(range(7, 10))
+    routes = [1]
+    bench = alg_bench.benchmark()
+    bench.benchmark_init(routes, window_range)
+
+
+if __name__ == "__main__":
+    main()
