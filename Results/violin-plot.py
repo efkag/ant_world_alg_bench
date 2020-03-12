1cb1e48a4745b93c15cb355f586d7d3bb5a17425
efkag <kagioulis.efstathios@gmail.com>
2020-03-12 12:30:23 +0000

    Add violin plot for matching methods

--- Results/violin-plot.py
+++ Results/violin-plot.py
@@ -5,19 +5,33 @@ import matplotlib.pyplot as plt
 # for partname in ('cbars','cmins','cmaxes','cmeans','cmedians')
 
 data = pd.read_csv('bench-results.csv')
+print(data.columns)
 # Convert list of strings to actual list of lists
 data['errors'] = pd.eval(data['errors'])
 
-# Use only window data
+# Plot violin by window size
 pos = data['window'].unique()
-v_data = data.groupby(['window'])['errors'].apply(list).tolist()
-#TODO need to flatten error arrays here
+v_data = data.groupby(['window'])['errors'].apply(sum).tolist()
 fig, ax = plt.subplots()
 parts = ax.violinplot(v_data, pos, points=100, widths=0.7, showmeans=True,
                       showextrema=True, showmedians=True, bw_method=0.5)
-# parts['cmeans'].set_edgecolor('red')
+parts['cmeans'].set_edgecolor('red')
 ax.set_ylabel('Degree error')
 ax.set_xlabel('Window size')
 plt.show()
 
+
+# Plot violin for idf v corr
+v_data = data.groupby(['matcher'])['errors'].apply(sum).tolist()
+labels = data['matcher'].unique()
+pos = np.arange(len(labels))
+fig, ax = plt.subplots()
+parts = ax.violinplot(v_data, pos, points=100, widths=0.3, showmeans=True,
+                      showextrema=True, showmedians=True, bw_method=0.5)
+parts['cmeans'].set_edgecolor('red')
+ax.set_ylabel('Degree error')
+ax.set_xlabel('Matcher type')
+ax.set_xticks(np.arange(len(labels)))
+ax.set_xticklabels(labels)
+plt.show()
 
