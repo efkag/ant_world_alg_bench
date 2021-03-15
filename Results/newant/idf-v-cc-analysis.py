5027ccbd029ef80f9d74dbd9ce46068a83f1b73f
efkag <kagioulis.efstathios@gmail.com>
2021-03-15 14:54:33 +0000

    Add code to plot the comparison between matchgin functions

---
+++ Results/newant/idf-v-cc-analysis.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import textwrap
+import seaborn as sns
+from ast import literal_eval
+sns.set_context("paper", font_scale=1)
+
+
+def to_array(x):
+    return np.fromstring(x[1:-1], dtype=np.int, sep=' ').tolist()
+
+fig_save_path = 'violins.png'
+data = pd.read_csv('combined-results.csv')
+# Convert list of strings to actual list of lists
+data['errors'] = data['errors'].apply(literal_eval)
+data['dist_diff'] = data['dist_diff'].apply(literal_eval)
+
+
+# for the lack of edges I have to use .isna() function
+edge = '(220, 240)'
+res = '(180, 50)'
+figsize = (4, 3)
+matchers = ['mae', 'corr']
+
+
+df = data.loc[(data['res'] == res) & (data['edge'] == edge)]
+'''
+Plot for idf v cc matchers with one specific resolution
+'''
+fig, ax = plt.subplots(figsize=figsize)
+window_labels = ['Adaptive (20)', 'PM', 'w=15', 'w=20', 'w=25', 'w=30']
+plt.title(res + edge)
+# Group then back to dataframe
+grouped = df.groupby(['window', 'matcher'])['errors'].apply(sum).to_frame('errors').reset_index()
+
+hue = []
+x = []
+y = []
+for m in matchers:
+    df = grouped.loc[grouped['matcher'] == m]['errors'].to_list()
+    for i, row in enumerate(df):
+        x.extend([i] * len(row))
+        hue.extend([m] * len(row))
+        y.extend(row)
+sns.violinplot(x=x, y=y, hue=hue, ax=ax, cut=0, split=True, inner="quart")
+plt.tight_layout(pad=0)
+ax.set_xticklabels(window_labels)
+plt.show()
+
+
+'''
+IDF with blur and CC with edges
+'''
+cc_df = data.loc[(data['res'] == res) & (data['matcher'] == 'corr') & (data['edge'] == edge)]
+cc_df = cc_df.groupby(['window'])['errors'].apply(sum).to_frame('errors').reset_index()
+idf_df = data.loc[(data['res'] == res) & (data['matcher'] == 'mae') & (data['edge'].isna())]
+idf_df = idf_df.groupby(['window'])['errors'].apply(sum).to_frame('errors').reset_index()
+fig, ax = plt.subplots(figsize=figsize)
+window_labels = ['Adaptive (20)', 'PM', 'w=15', 'w=20', 'w=25', 'w=30']
+plt.title('CC with edges and IDF with Blur')
+for m, df in zip(matchers, [idf_df, cc_df]):
+    df = df['errors'].to_list()
+    for i, row in enumerate(df):
+        x.extend([i] * len(row))
+        hue.extend([m] * len(row))
+        y.extend(row)
+sns.violinplot(x=x, y=y, hue=hue, ax=ax, cut=0, split=True, inner="quart")
+plt.tight_layout(pad=0)
+ax.set_xticklabels(window_labels)
+plt.show()
