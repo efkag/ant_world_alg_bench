4a71a95b1955a5f1633837a29c172dfb67f70695
efkag <kagioulis.efstathios@gmail.com>
2021-06-08 01:25:16 +0100

    Update pre processing functions comments

--- source/imgproc.py
+++ source/imgproc.py
@@ -5,18 +5,27 @@ import numpy as np
 # In the future I might need to make this into a variable as well
 kernel_shape = (5, 5)
 
+
 def resize(shape):
-    '''Return a function to resize image to given size'''
+    '''
+    Return a function to resize image to given size
+    '''
     return lambda im: cv.resize(im, shape)
 
 
 def gauss_blur(kernel_shape, mean):
-    '''Return a function to blur image given the kernel size and the mean'''
+    '''
+    Return a function to blur image
+    given the kernel size and the mean
+    '''
     return lambda im: cv.GaussianBlur(im, kernel_shape, mean)
 
 
 def canny(upper, lower):
-    '''Return a function to perform Canny edge detection within the given thresholds'''
+    '''
+    Return a function to perform Canny edge detection
+    within the given thresholds
+    '''
     return lambda im: cv.Canny(im, upper, lower)
 
 
