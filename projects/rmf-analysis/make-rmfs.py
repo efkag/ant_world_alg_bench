d65b2929623e64605633c02b8d31875cbdd50a06
efkag <kagioulis.efstathios@gmail.com>
2021-12-03 18:44:24 +0000

    Add new project folder and file to test RMFs quality

---
+++ projects/rmf-analysis/make-rmfs.py
@@ -0,0 +1,24 @@
+import sys
+import os
+path = os.path.join(os.path.dirname(__file__), os.pardir)
+fwd = os.path.dirname(__file__)
+cwd = os.getcwd()
+sys.path.append(cwd)
+
+
+import numpy as np
+import pandas as pd
+import cv2 as cv
+import matplotlib.pyplot as plt
+import pickle
+from source.display import plot_multiline, plot_3d
+from source.utils import rmf, cor_dist, save_image, rotate
+from source.routedatabase import Route
+
+
+path =  os.path.join(cwd, 'new_antworld', 'exp1', 'route1')
+print(path)
+# df = pd.read_csv(fwd + '/office/training.csv')
+# testdf = pd.read_csv(fwd + '/office/testing.csv')
+
+# route = df.to_dict('list')
