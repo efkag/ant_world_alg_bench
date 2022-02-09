4bf98667e9a990f1d5416e5c1b4b2cc3c8b1425f
efkag <kagioulis.efstathios@gmail.com>
2022-02-09 14:24:12 +0000

    Plots and results from the testing if SMW and PM

--- Results/newant/route-plot-errors-analysis.py
+++ Results/newant/route-plot-errors-analysis.py
@@ -12,8 +12,8 @@ from source.utils import load_route_naw, plot_route, animated_window, check_for_
 sns.set_context("paper", font_scale=1)
 
 
-fig_save_path = 'Results/newant/2022-01-27'
-data = pd.read_csv('Results/newant/2022-01-27/results.csv')
+fig_save_path = 'Results/newant/test'
+data = pd.read_csv('Results/newant/test/results.csv')
 # Convert list of strings to actual list of lists
 data['errors'] = data['errors'].apply(literal_eval)
 data['dist_diff'] = data['dist_diff'].apply(literal_eval)
@@ -24,11 +24,11 @@ data['th'] = data['th'].apply(literal_eval)
 
 
 # Plot a specific route
-route_id = 1
+route_id = 3
 fig_save_path = os.path.join(fig_save_path, str(route_id))
 check_for_dir_and_create(fig_save_path)
 path = 'new-antworld/exp1/route' + str(route_id) + '/'
-window = -15
+window = 0
 matcher = 'corr'
 edge = 'False'
 res = '(180, 50)'
