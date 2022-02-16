30ba07804d609b7c17c626730b8b28e95aeaa992
efkag <kagioulis.efstathios@gmail.com>
2022-02-16 18:56:45 +0000

    Add new placeholder file for use to evaluate RMFs

--- projects/rmf-analysis/eval-rmfs.py
+++ projects/rmf-analysis/eval-rmfs.py
@@ -13,23 +13,13 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 import pickle
 from source.display import plot_multiline, plot_3d
-from source.utils import rmf, cor_dist, save_image, rotate
+from source.utils import rmf, cor_dist, save_image
 from source.routedatabase import Route
 from source.analysis import flip_gauss_fit
 
-
 path =  os.path.join(cwd, 'new-antworld', 'exp1', 'route1')
 route = Route(path, route_id=1)
-# df = pd.read_csv(fwd + '/office/training.csv')
-# testdf = pd.read_csv(fwd + '/office/testing.csv')
 
-# route = df.to_dict('list')
 imgs = route.get_imgs()[:20]
 search_angle = (-90, 90)
 rsim = rmf(imgs[10], imgs[10], d_range=(-90, 90))
-
-g_curve = flip_gauss_fit(rsim, drange=search_angle)
-x = np.arange(len(rsim))
-plt.plot(x, rsim)
-plt.plot(x, g_curve)
-plt.show()
