4dadd74bfd3c9b00171a097690f06e28cdfc9113
efkag <kagioulis.efstathios@gmail.com>
2022-03-11 13:10:56 +0000

    Add script to test the correlation between the angluar error and MSE with the guassian fit

--- projects/rmf-analysis/local-trmf-angmetric-corr.py
+++ projects/rmf-analysis/local-trmf-angmetric-corr.py
@@ -9,8 +9,9 @@ sys.path.append(cwd)
 
 import numpy as np
 import matplotlib.pyplot as plt
-from source.utils import rmf, mean_angle, squash_deg, pre_process
+from source.utils import rmf, mean_angle, squash_deg, pre_process, weighted_mse
 from source.routedatabase import Route
+from source.analysis import flip_gauss_fit, gauss_curve
 
 path =  os.path.join(cwd, 'new-antworld', 'exp1', 'route1')
 route = Route(path, route_id=1)
@@ -19,26 +20,26 @@ sets = {'blur': True, 'shape': (180, 50)}
 imgs = pre_process(route.get_imgs(), sets)
 route_dict = route.get_route_dict()
 
-ang_err = np.load(os.path.join(fwd, 'ang_err.npy'))
-fit_err = np.load(os.path.join(fwd, 'fit_err.npy'))
-
-# plt.scatter(ang_err, fit_err)
-# for i, txt in enumerate(fit_err):
-#     plt.annotate(i, (ang_err[i], fit_err[i]))
-# plt.show()
-idx = np.argwhere(fit_err >= 500).flatten()
-
-deg_range = (-180, 180)
+deg_range = (-90, 90)
 degrees = np.arange(*deg_range)
 margin = 5
-ang_err = []
-mse = []
-for i in idx:
+gauss_mse = []
+ang_errors = []
+for i in range(10, 60):
     ref_h = route_dict['yaw'][i]
-    ref_im = imgs[i]
+    im = imgs[i]
     test_ims = imgs[i-margin:i]
     test_ims.extend(imgs[i+1:i+margin+1])
-    rsims = rmf(ref_im, test_ims, d_range=(deg_range))
+    rsims = rmf(im, test_ims, d_range=deg_range)
+
+    ref_rsim = rmf(im, im, d_range=deg_range)
+    gauss_rmf = flip_gauss_fit(ref_rsim, d_range=deg_range)
+    # evaluate 
+    gauss_weights = gauss_curve(ref_rsim, d_range=deg_range)
+    rsims = list(rsims)
+    fit_errors = weighted_mse(gauss_rmf, rsims, weights=gauss_weights)
+    gauss_mse.extend(fit_errors)
+    # Get the heading errors
     # indixes of the minima
     idxs = np.argmin(rsims, axis=1)
     h = []
@@ -47,17 +48,12 @@ for i in idx:
         new_h = ref_h + degrees[j]
         new_h = squash_deg(new_h)
         h.append(new_h)
-    
-    mse.extend([fit_err[i]]*len(idxs))
     diff_a = ref_h - h
     er = np.abs((diff_a + 180) % 360 - 180)
-    ang_err.extend(er)
+    ang_errors.extend(er)
 
-plt.scatter(ang_err, mse)
+
+plt.scatter(ang_errors, gauss_mse)
 plt.xlabel('angular error')
 plt.ylabel('MSE')
 plt.show()
-
-
-
-
