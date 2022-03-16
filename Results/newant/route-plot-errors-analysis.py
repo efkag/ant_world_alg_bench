5d157d3995869bc78410327bd58630d40e07a2cc
efkag <kagioulis.efstathios@gmail.com>
2022-03-16 14:57:27 +0000

    Modify antworlds2, Route and worker script. The agent can be stopped when too far from the route and put back at the point closest to the route

--- Results/newant/route-plot-errors-analysis.py
+++ Results/newant/route-plot-errors-analysis.py
@@ -12,8 +12,8 @@ from source.utils import load_route_naw, plot_route, animated_window, check_for_
 sns.set_context("paper", font_scale=1)
 
 
-fig_save_path = 'Results/newant/test2'
-data = pd.read_csv('Results/newant/test2/results.csv')
+fig_save_path = 'Results/newant/2022-03-15'
+data = pd.read_csv('Results/newant/2022-03-15/results.csv')
 # Convert list of strings to actual list of lists
 data['errors'] = data['errors'].apply(literal_eval)
 data['dist_diff'] = data['dist_diff'].apply(literal_eval)
@@ -28,9 +28,9 @@ route_id = 1
 fig_save_path = os.path.join(fig_save_path, str(route_id))
 check_for_dir_and_create(fig_save_path)
 path = 'new-antworld/exp1/route' + str(route_id) + '/'
-window = -20
-matcher = 'corr'
-edge = '(180, 200)'
+window = 15
+matcher = 'mae'
+edge = 'False'
 res = '(180, 50)'
 threshold = 0
 figsize = (10, 10)
