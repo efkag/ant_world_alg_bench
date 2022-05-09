e5ef51a9c3b577ab5b61c1f1e2fcec25d2a08317
efkag <kagioulis.efstathios@gmail.com>
2022-05-09 18:59:26 +0200

    Detect horison trend line and project edge points on the line.

--- projects/horizon-extraction/detect-horizon.py
+++ projects/horizon-extraction/detect-horizon.py
@@ -31,23 +31,54 @@ params = {'blur': True,
 pipe = Pipeline(**params)
 edges = pipe.apply(img)
 
-plt.imshow(edges, cmap='gray')
-plt.show()
+# plt.imshow(edges, cmap='gray')
+# plt.show()
 
 points = np.where(edges > 0)
-points = np.vstack((points[1], points[0]))
+points = np.vstack((points[1], points[0])).astype(np.float64)
 line = linregress(points[0], points[1])
 
 slope = line[0]
 inter = line[1]
 print(slope, inter)
 x = np.array([points[0].min(), points[0].max()])
-y = slope*x + inter
+y = slope*x + points[1].min()
 
 
 plt.scatter(points[0], points[1])
+#plt.plot(x, y, c='r')
+plt.show()
+
+def project_on_line(a, b, p):
+        ap = p-a
+        ab = b-a
+        projected = a + np.dot(ap,ab)/np.dot(ab,ab) * ab
+        return projected
+
+a_point = np.hstack((x[0], y[0]))
+b_point = np.hstack((x[1], y[1]))
+
+projections = np.zeros_like(points)
+for i in range(len(points[0])):
+        p = points[:, i]
+        proj = project_on_line(a_point, b_point, p)
+        projections[:, i] = proj
+
+plt.scatter(points[0], points[1])
+plt.scatter(projections[0], projections[1])
 plt.plot(x, y, c='r')
 plt.show()
 
+diffs = np.subtract(points, projections)
+l2s = np.linalg.norm(diffs, axis=0)
+
+# add the edge vector magnitude to the projected points
+for i,l in enumerate(l2s):
+        projections[:, i] = projections[:, i] +  l2s[i]
+
+plt.scatter(points[0], points[1])
+plt.scatter(projections[0], projections[1])
+plt.plot(x, y, c='r')
+plt.show()
 
 
