03b62c68b59595299e55e143df0d416420638ed7
efkag <kagioulis.efstathios@gmail.com>
2022-11-17 15:25:26 +0000

    Add figure sizes

--- projects/perf-metrics/metric-corr.py
+++ projects/perf-metrics/metric-corr.py
@@ -7,8 +7,10 @@ sys.path.append(os.getcwd())
 
 import numpy as np
 import pandas as pd
+import yaml
 import matplotlib.pyplot as plt
 from source.utils import check_for_dir_and_create
+from source.routedatabase import Route
 import seaborn as sns
 from ast import literal_eval
 sns.set_context("paper", font_scale=1)
@@ -23,13 +25,16 @@ data['dist_diff'] = data['dist_diff'].apply(literal_eval)
 data['abs_index_diff'] = data['abs_index_diff'].apply(literal_eval)
 data['divergence'] = data['divergence'].apply(literal_eval)
 data['mdivergence'] = data['divergence'].apply(np.mean)
+data['tx'] = data['tx'].apply(literal_eval)
+data['ty'] = data['ty'].apply(literal_eval)
+data['th'] = data['th'].apply(literal_eval)
 
 #### all data
 aae = data['mean_error']
 tfc = data['trial_fail_count']
 mdiv = data['mdivergence']
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(8, 6))
 ax.scatter(aae, tfc, label='aae-tfc')
 ax.set_xlabel('mean aae')
 ax.set_ylabel('tfc')
@@ -57,7 +62,7 @@ aae = data['mean_error']
 tfc = data['trial_fail_count']
 mdiv = data['mdivergence']
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(8, 6))
 ax.scatter(aae, tfc, label='aae-tfc')
 ax.set_xlabel('mean aae')
 ax.set_ylabel('tfc')
@@ -68,15 +73,29 @@ ax1.set_ylabel('mean divergence')
 ax1.legend(loc=1)
 plt.show()
 
+#### route with specific performance metric values
+# data = data.loc[data['mean_error'] > 60]
+# row = data.iloc[0].to_dict()
+# route_id = row['route_id']
+# with open(os.path.join(fig_save_path, 'params.yml')) as fp:
+#     params = yaml.load(fp)
+# routes_path = params['routes_path']
+# routepath = os.path.join(routes_path, f"route{route_id}")
+# route = Route(routepath, route_id=route_id)
+# route = route.get_route_dict()
+# traj = {'x': np.array(row['tx']),
+#         'y': np.array(row['ty']),
+#         'heading': np.array(row['th'])}
+
 #### Route specific
-route_id = 1
+route_id = 3
 # window = 0
 data = data.loc[data['route_id'] == route_id]
 aae = data['mean_error']
 tfc = data['trial_fail_count']
 mdiv = data['mdivergence']
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(8, 6))
 ax.scatter(aae, tfc, label='aae-tfc')
 ax.set_xlabel('mean aae')
 ax.set_ylabel('tfc')
