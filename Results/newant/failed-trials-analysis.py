8130da80413cecd26596a44c077f5f509a17282a
efkag <kagioulis.efstathios@gmail.com>
2023-03-28 13:52:27 +0100

    Modify plotting function for TFC

--- Results/newant/failed-trials-analysis.py
+++ Results/newant/failed-trials-analysis.py
@@ -12,14 +12,14 @@ from ast import literal_eval
 from source.utils import load_route_naw, plot_route, animated_window, check_for_dir_and_create
 sns.set_context("paper", font_scale=1)
 
-directory = '2023-01-25_mid_update'
+directory = '2023-03-24_allroutes_thresh_mid_update'
 fig_save_path = os.path.join('Results', 'newant', directory)
 data = pd.read_csv(os.path.join(fig_save_path, 'results.csv'), index_col=False)
 # data['trial_fail_count'] = data['trial_fail_count'].apply(literal_eval)
 
 
 # Choose a specific pre. processing
-route_id = 7
+route_id = None
 matcher = 'corr'
 blur = True
 # edge = 'False' 
@@ -33,9 +33,13 @@ data = data.loc[(data['matcher'] == matcher) & (data['res'] == res)
                 #& (data['edge'] == edge) 
                 & (data['gauss_loc_norm'] == g_loc_norm)
                 #& (data['loc_norm'] == loc_norm)
-                & (data['route_id'] == route_id )
+                #& (data['route_id'] == route_id )
                 #& (data['route_id'] > 9 ) #& (data['route_id'] <= 14 ) 
 ]
+#################
+# in case of repeats
+method = sum
+data = data.groupby(['window', 'route_id'])["trial_fail_count"].apply(method).to_frame("trial_fail_count").reset_index()
 
 figsize = (5, 3)
 fig, ax = plt.subplots(figsize=figsize)
@@ -45,6 +49,6 @@ sns.barplot(x="window", y="trial_fail_count", data=data, ax=ax, estimator=sum, c
 plt.tight_layout(pad=0)
 # path = os.path.join(fig_save_path, f'route[{route_id}]-failed trials.png')
 path = os.path.join(fig_save_path, 'curve1-failed trials.png')
-fig.savefig(path)
+#fig.savefig(path)
 plt.show()
 
