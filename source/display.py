1cc6a54ea4daddcdd1169c6090805da2b5ddc57b
efkag <kagioulis.efstathios@gmail.com>
2023-07-04 11:55:51 +0100

    plotting changes

--- source/display.py
+++ source/display.py
@@ -224,6 +224,61 @@ def plot_route(route, traj=None, scale=None, window=None, windex=None, save=Fals
     plt.close(fig)
 
 
+def plot_ftl_route(route, traj=None, scale=None, window=None, windex=None, save=False, size=(10, 10), path=None, title=None):
+    '''
+    Plots the route and any given test points if available.
+    Note the route headings are rotated 90 degrees as the 0 degree origin
+    for the FTL seesm to be east but for some reason it does not plot properly
+    :param route:
+    :param traj:
+    :param scale:
+    :param window:
+    :param windex:
+    :param save:
+    :param size:
+    :param path:
+    :param title:
+    :return:
+    '''
+    fig, ax = plt.subplots(figsize=size)
+    ax.set_title(title,  loc="left")
+    
+    u, v = pol2cart_headings(90 + route['yaw'])
+    ax.scatter(route['x'], route['y'])
+    ax.quiver(route['x'], route['y'], u, v, scale=scale)
+    if window is not None and windex:
+        start = window[0]
+        end = window[1]
+        ax.quiver(route['x'][start:end], route['y'][start:end], u[start:end], v[start:end], color='r', scale=scale)
+        if not traj:
+            ax.scatter(route['qx'][:windex], route['qy'][:windex])
+        else:
+            ax.scatter(traj['x'][:windex], traj['y'][:windex])
+            u, v = pol2cart_headings(90 + traj['heading'])
+            ax.quiver(traj['x'][:windex], traj['y'][:windex], u[:windex], v[:windex], scale=scale)
+    # Plot grid test points
+    if 'qx' in route and window is None:
+        ax.scatter(route['qx'], route['qy'])
+    # Plot the trajectory of the agent when repeating the route
+    if traj and not window:
+        # TODO: This re-correction (90 - headings) of the heading may not be necessary.
+        # TODO: I need to test if this will work as expected when the new results are in.
+        u, v = pol2cart_headings(90 + traj['heading'])
+        ax.scatter(traj['x'], traj['y'])
+        # ax.plot(traj['x'], traj['y'])
+        ax.quiver(traj['x'], traj['y'], u, v, scale=scale)
+    plt.axis('equal')
+    plt.tight_layout()
+    if save and windex:
+        fig.savefig(path + '/' + str(windex) + '.png')
+        plt.close(fig)
+    elif save:
+        fig.savefig(path)
+
+    if not save: plt.show()
+    plt.close(fig)
+
+
 def _plot_route_axis(route, ax, traj=None, scale=None, window=None, windex=None, size=(10, 10), title=None, **kwargs):
     ax.set_title(title,  loc="left")
     u, v = pol2cart_headings(90 - route['yaw'])
