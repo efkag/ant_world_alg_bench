c10763a5056ad472e6ae6e71ea3fe3a7fdb8ad35
efkag <kagioulis.efstathios@gmail.com>
2023-09-20 15:35:03 +0100

    Update paths

--- main.py
+++ main.py
@@ -38,14 +38,14 @@ def static_bench():
 
 
 def static_bench_antworld():
-    results_path = f'/its/home/sk526/ant_world_alg_bench/Results/newant/{string_date}'
-    #results_path = f'/mnt/data0/sk526/Results/aw/{string_date}'
+    #results_path = f'/its/home/sk526/ant_world_alg_bench/Results/newant/{string_date}'
+    results_path = f'/mnt/data0/sk526/Results/aw/{string_date}'
 
-    routes_path = '/its/home/sk526/ant_world_alg_bench/new-antworld/curve-bins'
-    #routes_path = '/mnt/data0/sk526/new-antworld/curve-bins'
+    #routes_path = '/its/home/sk526/ant_world_alg_bench/new-antworld/curve-bins'
+    routes_path = '/mnt/data0/sk526/new-antworld/curve-bins'
 
-    grid_path = '/its/home/sk526/ant_world_alg_bench/new-antworld/grid70'
-    #grid_path = '/mnt/data0/sk526/new-antworld/grid70'
+    #grid_path = '/its/home/sk526/ant_world_alg_bench/new-antworld/grid70'
+    grid_path = '/mnt/data0/sk526/new-antworld/grid70'
     # parameters = {'blur': [True], 'segment_l': [3], 'shape': [(180, 50), (90, 25)], 'edge_range': [(180, 200)],
     #               'window': list(range(10, 12)), 'matcher': ['corr', 'rmse']}
     
@@ -60,7 +60,7 @@ def static_bench_antworld():
                   'grid_dist':[0.2]
                   }
     
-    routes = [1]
+    routes = [*range(20)]
     bench = navbench.Benchmark(results_path, routes_path, 
                                grid_path=grid_path, grid_dist=0.2,
                                filename='results.csv'
