2925bdb259e536b21fd46789b6223f62d2d88ebe
efkag <kagioulis.efstathios@gmail.com>
2023-10-05 16:35:41 +0100

    update plots

--- projects/FTL_live_tests/plot-ftl-trial.py
+++ projects/FTL_live_tests/plot-ftl-trial.py
@@ -26,8 +26,10 @@ def load_testing_logs(route_path, dname):
 route_id=2
 pm_logs = ['pm0', 'pm1', 'pm2', 'pm3', 'pm4'] 
 asmw_logs = ['asmw0', 'asmw1', 'asmw2', 'asmw3', 'asmw4'] 
+title = 'A'
+trial_logs = pm_logs
 
-route_path = os.path.join(fwd,'2023-09-11', f'route{route_id}')
+route_path = os.path.join(fwd,'2023-10-03', f'route{route_id}')
 fig_save_path = os.path.join(route_path, 'analysis')
 check_for_dir_and_create(fig_save_path)
 route_data = os.path.join(route_path, 'database_entries.csv')
@@ -39,7 +41,7 @@ route['x'] = np.array(route.pop('X [mm]'))
 route['y'] = np.array(route.pop(' Y [mm]'))
 route['yaw'] = np.array(route.pop(' Heading [degrees]'))
 
-background = cv2.imread(os.path.join(fwd, "top-down.png"))
+background = cv2.imread(os.path.join(fwd, "top-down-fliped.png"))
 background = cv2.cvtColor(background, cv2.COLOR_BGR2RGB)
 
 
@@ -51,12 +53,12 @@ Here all the xy are flipped and rotated by 270 degreee to  plot aproaproiately
 '''
 
 
-fig = plt.figure(figsize=(3.5, 3.5))
-#plt.title('C', loc='left')
-plt.plot(-route['y'], -route['x'], label='training')
+fig = plt.figure(figsize=(3, 3))
+plt.title(title, loc='left')
+plt.plot(route['x'], route['y'], label='training')
 
-plt.scatter(-route['y'][0], -route['x'][0])
-plt.annotate('Start', (-route['y'][0], -route['x'][0]))
+plt.scatter(route['x'][0], route['y'][0])
+plt.annotate('Start', (route['x'][0], route['y'][0]))
 # Show background
 # **NOTE** the extents should correspond to EXPERIMENT_AREA_X and EXPERIMENT_AREA_Y in aligner.py
 plt.imshow(background, extent=(-3000.0, 3000.0, 3000.0, -3000.0))
@@ -70,27 +72,33 @@ plt.yticks([])
 Plot trials
 '''
 
-logs_path = os.path.join(route_path, 'testing')
-for i, log in enumerate(pm_logs):
-    r = load_testing_logs(logs_path, log)
-    # if i==0:
-    # use the label for the legend if it is the first iteration
-    plt.plot(-r['y'], -r['x'], ':', label=f'pm{i}')
-    # else:
-    #     plt.plot(-r['y'], -r['x'], ':')
-    plt.scatter(-r['y'][-1], -r['x'][-1])
-    #plt.annotate('pm{} ends'.format(i), (r['x'][-1], r['y'][-1]))
+# logs_path = os.path.join(route_path, 'testing')
+# for i, log in enumerate(pm_logs):
+#     r = load_testing_logs(logs_path, log)
+#     # if i==0:
+#     # use the label for the legend if it is the first iteration
+#     plt.plot(-r['y'], -r['x'], ':', label=f'pm{i}')
+#     # else:
+#     #     plt.plot(-r['y'], -r['x'], ':')
+#     plt.scatter(-r['y'][-1], -r['x'][-1])
+#     #plt.annotate('pm{} ends'.format(i), (r['x'][-1], r['y'][-1]))
 
+if 'pm' in trial_logs[0]:
+    navalg = 'pm'
+elif 'asmw' in trial_logs[0]:
+    navalg = 'asmw'
+else:
+    raise Exception('Probably wrong trial directory name')
 
 logs_path = os.path.join(route_path, 'testing')
-for i, log in enumerate(asmw_logs):
+for i, log in enumerate(trial_logs):
     r = load_testing_logs(logs_path, log)
     # use the label for the legend if it is the first iteration
     # if i==0:
-    plt.plot(-r['y'], -r['x'], '--', label=f'asmw{i}')
+    plt.plot(r['x'], r['y'], '--', label=f'{navalg}{i}')
     # else:
     #     plt.plot(-r['y'], -r['x'], '--')
-    plt.scatter(-r['y'][-1], -r['x'][-1])
+    plt.scatter(r['x'][-1], r['y'][-1])
     #plt.annotate('asmw{} ends'.format(i), (r['x'][-1], r['y'][-1]))
 
 
@@ -99,5 +107,6 @@ for i, log in enumerate(asmw_logs):
 
 plt.legend()
 plt.tight_layout()
-fig.savefig(os.path.join(fig_save_path, f'large-r({route_id})-pm-trials.png'))
+fig.savefig(os.path.join(fig_save_path, f'r({route_id})-{trial_logs}.png'))
+fig.savefig(os.path.join(fig_save_path, f'r({route_id})-{trial_logs}.pdf'))
 plt.show()
