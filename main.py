b83dd5fd1639cfc24da425ea1a5f3ab7001bd40f
efkag <kagioulis.efstathios@gmail.com>
2023-10-06 16:07:56 +0100

    Modify main

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from source import navbench
 
 
 def static_bench():
-    results_path = f'/its/home/sk526/ant_world_alg_bench/Results/ftl/{string_date}'
-    #results_path = f'/mnt/data0/sk526/Results/ftl/{string_date}'
+    #results_path = f'/its/home/sk526/ant_world_alg_bench/Results/ftl/{string_date}'
+    results_path = f'/mnt/data0/sk526/Results/ftl/{string_date}'
     
     #routes_path = '/its/home/sk526/sussex-ftl-dataset/repeating-routes'
     #routes_path = '/mnt/data0/sk526/sussex-ftl-dataset/repeating-routes'
-    routes_path = '/its/home/sk526/ftl-trial-repeats'
+    #routes_path = '/its/home/sk526/ftl-trial-repeats'
+    routes_path = '/mnt/data0/sk526/ftl-trial-repeats'
     # grid_path = '/home/efkag/PycharmProjects/ant_world_alg_bench/new-antworld/grid70'
     # parameters = {'blur': [True], 'segment_l': [3], 'shape': [(180, 50), (90, 25)], 'edge_range': [(180, 200)],
     #               'window': list(range(10, 12)), 'matcher': ['corr', 'rmse']}
