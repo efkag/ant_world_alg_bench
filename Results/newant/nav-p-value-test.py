e568f112320baad59edaa5da0501fc78ccd488ed
efkag <kagioulis.efstathios@gmail.com>
2023-11-14 14:33:14 +0000

    script to create specific data matrix from bench data

---
+++ Results/newant/nav-p-value-test.py
@@ -0,0 +1,66 @@
+import sys
+import os
+# path = os.path.join(os.path.dirname(__file__), os.pardir)
+fwd = os.path.dirname(__file__)
+sys.path.append(os.getcwd())
+
+import pandas as pd
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+import seaborn as sns
+from ast import literal_eval
+from source.utils import load_route_naw, plot_route, animated_window, check_for_dir_and_create
+sns.set_context("paper", font_scale=1)
+
+directory = '2023-04-26/combined'
+results_path = os.path.join('Results', 'newant', directory)
+fig_save_path = os.path.join(results_path, 'analysis')
+check_for_dir_and_create(fig_save_path)
+data = pd.read_csv(os.path.join(results_path, 'results.csv'), index_col=False)
+data['errors'] = data['errors'].apply(literal_eval)
+
+
+# Choose a specific pre. processing
+# route_id = 7
+matcher = 'corr'
+blur = True
+# edge = 'False' 
+res = '(180, 80)'
+g_loc_norm = "{'sig1': 2, 'sig2': 20}"
+# loc_norm = 'False'
+title = None
+
+# imax_df = data.loc[data['nav-name'] == 'InfoMax']
+# data = pd.concat([data, imax_df])
+
+data = data.loc[(data['matcher'] == matcher) 
+                & (data['res'] == res) 
+                & (data['blur'] == blur) 
+                #& (data['edge'] == edge) 
+                & (data['gauss_loc_norm'] == g_loc_norm)
+                #& (data['loc_norm'] == loc_norm)
+                #& (data['route_id'] == route_id ) 
+                # & (data['num_of_repeat'] == 0) 
+             #& (data['route_id'] <= 4 ) #& (data['route_id'] < 10 )
+                
+]
+
+
+
+#################
+# in case of repeats
+method = np.mean
+   
+#data = data.groupby(['window', 'route_id'])["trial_fail_count"].apply(method).to_frame("trial_fail_count").reset_index()
+##### if the dataset had nav-names
+grouped = data.groupby(['route_id', 'nav-name'])["trial_fail_count"].apply(method).to_frame("trial_fail_count").reset_index()
+#data['errors'] = data['errors'].apply(np.array)
+
+#data['mean_aae'] = np.mean(data['errors'])
+
+grouped = grouped.pivot('route_id', 'nav-name', 'trial_fail_count')
+grouped.to_csv(os.path.join(fig_save_path, 'grouped.csv'), index=False)
+
+# pm = 
+#use stats.ttest_ind(pm, asmw, equal_var=False)
--- Results/newant/static-bench/aae-route-plot.py
+++ Results/newant/static-bench/aae-route-plot.py
@@ -12,32 +12,33 @@ from ast import literal_eval
 sns.set_context("paper", font_scale=1)
 
 
-
-#fig_save_path = '/home/efkag/Desktop/perf'
-fig_save_path = os.path.join(fwd, 'analysis')
+directory = '2023-11-01'
+results_path = os.path.join('Results', 'newant', 'static-bench', directory)
+fig_save_path = os.path.join(results_path, 'analysis')
 check_for_dir_and_create(fig_save_path)
-results_path = os.path.join(fwd, 'combined-results2.csv')
-data = pd.read_csv(results_path)
-# data = pd.read_csv('exp4.csv')
+data = pd.read_csv(os.path.join(results_path, 'results.csv'), index_col=False)
+print(data)
+######################################################################
 # Convert list of strings to actual list of lists
 data['errors'] = data['errors'].apply(literal_eval)
 data['dist_diff'] = data['dist_diff'].apply(literal_eval)
 data['abs_index_diff'] = data['abs_index_diff'].apply(literal_eval)
 
-route_id = 1
+route_id = 18
 # fig_save_path = fig_save_path + str(route_id)
 # check_for_dir_and_create(fig_save_path)
 matcher = 'mae'
-edge = 'False'#'(220, 240)'  # 'False'
+edge = 'False' # (180, 220)'  # 'False'
+g_loc_norm = 'False' #"{'sig1': 2, 'sig2': 20}"
 blur = True
 figsize = (6, 3)
-res = '(180, 50)'
+res = '(180, 40)'
 route = data.loc[(data['matcher'] == matcher) 
                  & (data['route_id'] == route_id)
-                 & (data['edge'] == edge) 
+                 & (data['edge'] == edge)
+                 & (data['gauss_loc_norm'] == g_loc_norm)
                  & (data['res'] == res) 
                  & (data['blur'] == blur)]
-window_labels = ['Adaptive (20)', 'PM', 'w=15', 'w=20', 'w=25', 'w=30']
 
 '''
 Plot for one specific matcher with one specific pre-proc
@@ -45,14 +46,15 @@ Plot for one specific matcher with one specific pre-proc
 fig, ax = plt.subplots(figsize=figsize)
 #plt.title(matcher + ', route:' + str(route_id))
 # Group then back to dataframe
-df = route.groupby(['window'])['errors'].apply(sum).to_frame('errors').reset_index()
+df = route.groupby(['nav-name'])['errors'].apply(sum).to_frame('errors').reset_index()
 df = df.explode('errors')
 df['errors'] = pd.to_numeric(df['errors'])
 #v_data = df['errors'].tolist()
 # Here i use index 0 because the tolist() func above returns a single nested list
-sns.violinplot(data=df, x='window', y='errors', cut=0, ax=ax)
+sns.violinplot(data=df, x='nav-name', y='errors', cut=0, ax=ax)
 # labels = df['window'].tolist()
 #ax.set_xticklabels(window_labels)
+ax.set_ylim(0, 180)
 ax.set_ylabel('AAE')
 ax.set_xlabel('navigation algorithm')
 plt.tight_layout()
@@ -66,21 +68,21 @@ plt.show()
 '''
 Plot for one specific matcher with one specific pre-proc
 '''
-missmatch_metric = 'dist_diff'
-# missmatch_metric = 'abs_index_diff'
-title = 'B'
+# missmatch_metric = 'dist_diff'
+# # missmatch_metric = 'abs_index_diff'
+# title = 'B'
 
-fig, ax = plt.subplots(figsize=figsize)
-#plt.title(title, loc="left")
-# Group then back to dataframe
-df = route.groupby(['window'])[missmatch_metric].apply(sum).to_frame(missmatch_metric).reset_index()
-v_data = df[missmatch_metric].tolist()
-# Here i use index 0 because the tolist() func above returns a single nested list
-sns.violinplot(data=v_data, cut=0, ax=ax)
-# labels = df['window'].tolist()
-ax.set_xticklabels(window_labels)
-ax.set_ylabel(missmatch_metric)
-ax.set_xlabel('Window size')
-plt.tight_layout(pad=0)
-fig.savefig(fig_save_path + '/{}.{}.route{}.png'.format(missmatch_metric, matcher, route_id))
-plt.show()
+# fig, ax = plt.subplots(figsize=figsize)
+# #plt.title(title, loc="left")
+# # Group then back to dataframe
+# df = route.groupby(['window'])[missmatch_metric].apply(sum).to_frame(missmatch_metric).reset_index()
+# v_data = df[missmatch_metric].tolist()
+# # Here i use index 0 because the tolist() func above returns a single nested list
+# sns.violinplot(data=v_data, cut=0, ax=ax)
+# # labels = df['window'].tolist()
+# ax.set_xticklabels(window_labels)
+# ax.set_ylabel(missmatch_metric)
+# ax.set_xlabel('Window size')
+# plt.tight_layout(pad=0)
+# fig.savefig(fig_save_path + '/{}.{}.route{}.png'.format(missmatch_metric, matcher, route_id))
+# plt.show()
