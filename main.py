c4b60b2c15f003ea42425d08b57addbd30130095
efkag <kagioulis.efstathios@gmail.com>
2023-12-11 18:56:18 +0000

    Update routedatabase to unwrap images as they are read in

--- main.py
+++ main.py
@@ -6,40 +6,40 @@ from source import navbench
 
 
 def static_bench():
-    # results_path = f'/its/home/sk526/ant_world_alg_bench/Results/ftl/{string_date}'
-    results_path = f'/mnt/data0/sk526/Results/ftl/imax{string_date}'
+    results_path = f'/its/home/sk526/ant_world_alg_bench/Results/stanmer/{string_date}'
+    #results_path = f'/mnt/data0/sk526/results/stanmer/{string_date}'
     
     #routes_path = '/its/home/sk526/sussex-ftl-dataset/repeating-routes'
     #routes_path = '/mnt/data0/sk526/sussex-ftl-dataset/repeating-routes'
-    # routes_path = '/its/home/sk526/ftl-trial-repeats/asmw-trials'
-    routes_path = '/mnt/data0/sk526/ftl-trial-repeats/asmw-trials'
+    routes_path = '/its/home/sk526/navlib/data/outdoors/clean/stanmer'
+    #routes_path = '/mnt/data0/sk526/ftl-trial-repeats/asmw-trials'
     # grid_path = '/home/efkag/PycharmProjects/ant_world_alg_bench/new-antworld/grid70'
     # parameters = {'blur': [True], 'segment_l': [3], 'shape': [(180, 50), (90, 25)], 'edge_range': [(180, 200)],
     #               'window': list(range(10, 12)), 'matcher': ['corr', 'rmse']}
     
-    parameters = {'blur': [False], 
-                  'shape': [(180, 50)],
-                  'vcrop':[.5],
-                  'histeq':[True],
-                  #'edge_range': [(180, 200), False],
+    parameters = {'blur': [True], 
+                  'shape': [(360, 90)],
+                  #'vcrop':[.5],
+                  #'histeq':[True],
+                  'edge_range': [(180, 200), False],
                   #'loc_norm': [{'kernel_shape':(3, 3)}, False],
-                  #'gauss_loc_norm': [{'sig1':2, 'sig2':20}, False],
+                  'gauss_loc_norm': [{'sig1':2, 'sig2':20}, False],
                   'deg_range':[(-90, 90)],
-                  'window': [None], 
-                  'matcher': [None],
-                  'ref_route': [1],
-                  'sample_step':[1]
+                  'window': [0, 15, 20, -15], 
+                  'matcher': ['mae'],
+                  'ref_route': [1, 2],
+                  #'sample_step':[1]
                   }
     
-    routes = [3]
+    routes = [1]
     bench = navbench.Benchmark(results_path, routes_path, 
                                grid_path=None, 
                                filename='results.csv',
-                               route_path_suffix='N-',
-                               route_repeats=3,
-                               bench_data='ftl'
+                               route_path_suffix='r',
+                               route_repeats=2,
+                               bench_data='bob'
                                )
-    bench.benchmark(parameters, routes, parallel=True, cores=2)
+    bench.benchmark(parameters, routes, parallel=True, cores=1)
 
 
 
@@ -104,8 +104,8 @@ def live_bench():
 
 
 def main():
-    #static_bench()
-    static_bench_antworld()
+    static_bench()
+    #static_bench_antworld()
     #live_bench()
 
 if __name__ == "__main__":
