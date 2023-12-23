7d05413f91fa63d9e69a6d2eca6c035425d95378
efkag <kagioulis.efstathios@gmail.com>
2023-12-23 15:28:00 +0200

    Specify the gaussin blur kernel size.

--- examples/show-gaussina-blur-kernel.py
+++ examples/show-gaussina-blur-kernel.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import cv2 as cv
 sns.set_context("paper", font_scale=1)
 
-route_path = 'new-antworld/exp1/route1/'
+route_path = 'datasets/new-antworld/exp1/route1/'
 route = Route(route_path, 1)
 
 save_path = os.path.join(fwd, 'rmf-curves')
@@ -23,7 +23,7 @@ q_img = route.get_imgs()[q_img_idx]
 ref_imgs = route.get_imgs()[q_img_idx - search_margin:q_img_idx + search_margin]
 
 blank = np.zeros_like(q_img)
-blank = np.zeros((10, 10))
+blank = np.zeros((61, 61))
 shape = blank.shape
 x = shape[0] // 2
 y = shape[1] // 2
@@ -32,13 +32,12 @@ blank[x, y] = 1
 
 # plt.imshow(blank, cmap='hot')
 # plt.show()
-
-k = cv.GaussianBlur(blank, (5, 5), 0)
-
-plt.imshow(k, cmap='hot')
-plt.show()
-
-k = cv.getGaussianKernel(5, 0)
+k = cv.GaussianBlur(blank, (0,0), 20)
 print(k)
 plt.imshow(k, cmap='hot')
 plt.show()
+
+k = cv.getGaussianKernel(11, 2)
+print(k)
+# plt.imshow(k, cmap='hot')
+# plt.show()
