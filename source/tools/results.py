3f0d4032f9d3332d2fa9091b5b2cf615261ea324
efkag <kagioulis.efstathios@gmail.com>
2024-02-08 19:32:50 +0000

    Fill results dataframes with False literals

--- source/tools/results.py
+++ source/tools/results.py
@@ -4,11 +4,11 @@ from ast import literal_eval
 def read_results(path: str):
     data = pd.read_csv(path, index_col=False)
     # if columns have been left with Nan Values replace then with False
-    data.fillna('False', inplace=True)
+    data.fillna(False, inplace=True)
     # Coloumns that we want to have
     keys = ['errors', 'dist_diff', 'abs_index_diff', 'index_diff',  'tx', 'ty', 'th', 
             'matched_index', 'min_dist_index', 'window_log', 'best_sims', 
-            'tfc_idxs']
+            'tfc_idxs', 'deg_range']
     # Convert list of strings to actual list of lists
     for k in keys:
         if k in data.columns:
