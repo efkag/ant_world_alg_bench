442ac3cb9c74e456a88929cda9102462ae88372f
efkag <kagioulis.efstathios@gmail.com>
2024-04-24 11:10:28 +0100

    RMF using pytorch. MAE and dot product distance.

--- source/tools/torchmatchers.py
+++ source/tools/torchmatchers.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 
 def rotate(d, image):
@@ -23,4 +24,45 @@ def mae(a, b):
     :param b: One or more reference images
     :return:
     """
-    return torch.mean(torch.abs(a - b), dim=-1)
+    return torch.mean(torch.abs(torch.sub(a, b)), axis=(1, 2)).detach().cpu().numpy()
+
+
+def dot_dist(a, b):
+    """
+    Returns the dot product distance.
+    This function assumes the vectors have zero means and unit variance.
+    :param a: numpy vector or matrix
+    :param b: numpy vector or matrix
+    :return: distance between [0, 2]
+    """
+    #a = torch.unsqueeze(a, 0)
+    a = a.flatten()
+    b = b.flatten(start_dim=1)
+    res = 1 - torch.matmul(b, a)
+    return res.detach().cpu().numpy()
+
+def rmf(query_img, ref_imgs, matcher=mae, d_range=(-180, 180), d_step=1):
+    """
+    Rotational Matching Function.
+    Rotates a query image and compares it with one or more reference images
+    :param query_img:
+    :param ref_imgs:
+    :param matcher:
+    :param d_range:
+    :param d_step:
+    :return:
+    """
+    #query_img = query_img.cuda()
+    if ref_imgs.ndim < 3:
+      torch.unsqueeze(ref_imgs, 0)
+
+    degrees = range(*d_range, d_step)
+    total_search_angle = round((d_range[1] - d_range[0]) / d_step)
+    ridfs = np.empty((len(ref_imgs), total_search_angle), dtype=np.float32)
+
+    for i, rot in enumerate(degrees):
+        # rotated query image
+        rqimg = rotate(rot, query_img)
+        ridfs[:, i] = matcher(rqimg, ref_imgs)
+
+    return ridfs if ridfs.shape[0] > 1 else ridfs[0]
