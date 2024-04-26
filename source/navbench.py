a31f590623de9679d9cd3ee0b132d6cc4b885823
efkag <kagioulis.efstathios@gmail.com>
2024-04-26 11:19:42 +0100

    Update single core benchmark so it can work with pytorch

--- source/navbench.py
+++ source/navbench.py
@@ -159,76 +159,141 @@ class Benchmark:
         return logs
 
     def bench_singe_core(self, params, route_ids=None):
-        self._total_jobs(params)
+        # save the parmeters of the test in a json file
+        check_for_dir_and_create(self.results_path)
+        check_for_dir_and_create(os.path.join(self.results_path, 'metadata'))
+        param_path = os.path.join(self.results_path, 'params.yml')
+        temp_params = copy.deepcopy(params)
+        temp_params['routes_path'] = self.routes_path
+        temp_params['route_ids'] = route_ids
+        with open(param_path, 'w') as fp:
+            yaml.dump(temp_params, fp)
 
-        # Get list of parameter keys
-        keys = [*params]
+        grid = self.get_grid_dict(params)
+        self.total_jobs = len(grid)
+        self.total_jobs * len(route_ids)
+        print('{} combinations, testing on {} routes, running on 1 core'.format(self.total_jobs, len(route_ids)))
+        print('data path: ', self.routes_path)
+        print('save path: ', self.results_path)
 
-        grid = itertools.product(*[params[k] for k in params])
+        log = {'route_id': [],'ref_route':[], 'rep_id': [], 'nav-name':[], 'sample_rate':[], 'blur': [], 
+            'histeq':[], 'edge': [], 'res': [], 'vcrop':[],'window': [], 'matcher': [],
+            'deg_range':[], 'mean_error': [], 'seconds': [], 'errors': [], 
+            'index_diff': [], 'window_log': [], 'matched_index': [], 'min_dist_index': [],
+            'dist_diff': [], 'tx': [], 'ty': [], 'th': [],'ah': [] , 'rmfs_file':[], 'best_sims':[], 
+            'loc_norm':[], 'gauss_loc_norm':[], 'wave':[]}
+
+        routes = load_all_bob_routes(self.routes_path, route_ids, suffix=self.route_path_suffix, repeats=self.route_repeats)
 
         #  Go though all combinations in the grid
         for combo in grid:
 
-            # create combo dictionary
-            combo_dict = {}
-            for i, k in enumerate(keys):
-                combo_dict[k] = combo[i]
-
-            matcher = combo_dict['matcher']
-            window = combo_dict['window']
+            matcher = combo.get('matcher')
+            window = combo.get('window')
             window_log = None
-            path = '../new-antworld/'
-            for route_id in route_ids:  # for every route
-                # TODO: In the future the code below (inside the loop) should all be moved inside the navigator class
-                route_path = '../new-antworld/route' + str(route_id) + '/'
-                # route = load_route_naw(route_path, route_id=route_id, imgs=True, query=True, max_dist=0.2)
-                # _, test_x, test_y, test_imgs, route_x, route_y, \
-                #     route_heading, route_imgs = load_route(route, self.dist)
-                route = Route(route_path, route_id, grid_path=self.grid_path)
-                
-                tic = time.perf_counter()
-                # Preprocess images
-                test_imgs = pre_process(route.get_qimgs(), combo_dict)
-                route_imgs = pre_process(route.get_imgs(), combo_dict)
-                # Run navigation algorithm
-                if window:
-                    nav = spm.SequentialPerfectMemory(route_imgs, matcher, window=window)
-                    recovered_heading, window_log = nav.navigate(test_imgs)
-                else:
-                    nav = pm.PerfectMemory(route_imgs, matcher)
-                    recovered_heading = nav.navigate(test_imgs)
 
-                toc = time.perf_counter()
-                # Get time complexity
-                time_compl = toc-tic
-                # Get the errors and the minimum distant index of the route memory
-                # errors, min_dist_index = degree_error(test_x, test_y, route_x, route_y, route_heading, recovered_heading)
-                traj = {'x': route['qx'], 'y': route['qy'], 'heading': recovered_heading}
-                errors, min_dist_index = route.calc_errors(traj)
-                # Difference between matched index and minimum distance index and distance between points
-                matched_index = nav.get_index_log()
-                abs_index_diffs = np.absolute(np.subtract(nav.get_index_log(), min_dist_index))
-                dist_diff = calc_dists(route.get_xycoords(), min_dist_index, matched_index)
-                mean_route_error = np.mean(errors)
-                self.log['route_id'].extend([route_id])
-                self.log['blur'].extend([combo_dict.get('blur')])
-                self.log['edge'].extend([combo_dict.get('edge_range')])
-                self.log['res'].append(combo_dict.get('shape'))
-                self.log['window'].extend([window])
-                self.log['matcher'].extend([matcher])
-                self.log['mean_error'].append(mean_route_error)
-                self.log['seconds'].append(time_compl)
-                self.log['window_log'].append(window_log)
-                self.log['best_sims'].append(nav.get_best_sims())
-                self.log['tx'].append(traj['x'].tolist())
-                self.log['ty'].append(traj['y'].tolist())
-                self.log['th'].append(traj['heading'].tolist())
-                self.log['abs_index_diff'].append(abs_index_diffs.tolist())
-                self.log['dist_diff'].append(dist_diff.tolist())
-                self.log['errors'].append(errors)
+            for ri, route in enumerate(routes):  # for every route
+                #print('ref route -> ', combo['ref_route'])
+                ref_rep = route[combo['ref_route']]
+                ref_rep.set_sample_step(combo['sample_step'])
+                repeat_ids = [*range(1, self.route_repeats+1)]
+                #print(repeat_ids)
+                repeat_ids.remove(combo['ref_route'])
+                #print(repeat_ids)
+                for rep_id in repeat_ids: # for every repeat route
+                    test_rep = route[rep_id]
+                    test_rep.set_sample_step(combo['sample_step'])
+                    tic = time.perf_counter()
+                    # Preprocess images
+                    pipe = Pipeline(**combo)
+                    route_imgs = pipe.apply(ref_rep.get_imgs())
+                    test_imgs = pipe.apply(test_rep.get_imgs())
+                
+                    tic = time.perf_counter()
+                    # Preprocess images
+                    pipe = Pipeline(**combo)
+                    route_imgs = pipe.apply(ref_rep.get_imgs())
+                    test_imgs = pipe.apply(test_rep.get_imgs())
+
+                    if window:
+                        nav = spm.SequentialPerfectMemory(route_imgs, matcher, **combo)
+                        recovered_heading, window_log = nav.navigate(test_imgs)
+                    elif window == 0:
+                        nav = pm.PerfectMemory(route_imgs, **combo)
+                        recovered_heading = nav.navigate(test_imgs)
+                    else:
+                        infomaxParams = infomax.Params()
+                        nav = infomax.InfomaxNetwork(infomaxParams, route_imgs, **combo)
+                        recovered_heading = nav.navigate(test_imgs)
+
+                    toc = time.perf_counter()
+
+                    time_compl = toc - tic
+                    # Get the errors and the minimum distant index of the route memory
+                    qxy = test_rep.get_xycoords()
+                    traj = {'x': qxy['x'], 'y': qxy['y'], 'heading': recovered_heading}
+
+                    #################!!!!!! Important step to get the heading in the global coord system
+                    traj['heading'] = squash_deg(test_rep.get_yaw() + recovered_heading)
+                    errors, min_dist_index = ref_rep.calc_aae(traj)
+                    # Difference between matched index and minimum distance index and distance between points
+                    matched_index = nav.get_index_log()
+                    if matched_index:
+                        index_diffs = np.subtract(min_dist_index, nav.get_index_log())
+                        dist_diff = calc_dists(ref_rep.get_xycoords(), min_dist_index, matched_index)
+                        index_diffs = index_diffs.tolist()
+                        dist_diff = dist_diff.tolist()
+                    else:
+                        index_diffs = None
+                        dist_diff = None
+                    mean_route_error = np.mean(errors)
+                    window_log = nav.get_window_log()
+                    rec_headings = nav.get_rec_headings()
+                    deg_range = nav.deg_range
+
+                    #TODO check if we have empty logs from PM and omit saving those arrays
+                    # check if empty arrays cause saving problems.
+                    #rmf_logs = np.array(nav.get_rsims_log(), dtype=object)
+                    rmf_logs_file = f"rmfs-{self.jobs}_{uuid.uuid4().hex}"
+                    #rmfs_path = os.path.join(results_path, 'metadata', rmf_logs_file)
+                    #np.save(rmfs_path, rmf_logs)
+
+                    log['nav-name'].append(nav.get_name())
+                    log['route_id'].append(ri)
+                    log['ref_route'].append(combo.get('ref_route'))
+                    log['rep_id'].append(test_rep.get_route_id())
+                    log['sample_rate'].append(combo.get('sample_step'))
+                    log['blur'].append(combo.get('blur'))
+                    log['histeq'].append(combo.get('histeq'))
+                    log['edge'].append(combo.get('edge_range'))
+                    log['res'].append(combo.get('shape'))
+                    log['vcrop'].append(combo.get('vcrop'))
+                    log['window'].append(window)
+                    log['loc_norm'].append(combo.get('loc_norm'))
+                    log['gauss_loc_norm'].append(combo.get('gauss_loc_norm'))
+                    log['wave'].append(combo.get('wave'))
+                    log['matcher'].append(matcher)
+                    log['deg_range'].append(deg_range)
+                    log['mean_error'].append(mean_route_error)
+                    log['seconds'].append(time_compl)
+                    log['window_log'].append(window_log)
+                    log['tx'].append(traj['x'].tolist())
+                    log['ty'].append(traj['y'].tolist())
+                    # This is the heading in the global coord system
+                    log['th'].append(traj['heading'].tolist())
+                    # This is the agent heading from the egocentric agent reference
+                    log['ah'].append(rec_headings)
+                    log['rmfs_file'].append(rmf_logs_file)
+                    log['matched_index'].append(matched_index)
+                    log['min_dist_index'].append(min_dist_index)
+                    log['index_diff'].append(index_diffs)
+                    log['dist_diff'].append(dist_diff)
+                    log['errors'].append(errors)
+                    log['best_sims'].append(nav.get_best_sims())
             self.jobs += 1
+            print(datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"))
             print('Jobs completed: {}/{}'.format(self.jobs, self.total_jobs))
-        return self.log
+        return log
 
     def _total_jobs(self, params):
         for k in params:
