9b43978d6ac98e9f0c1f83e1caba2b5e0df2e596
efkag <kagioulis.efstathios@gmail.com>
2024-06-27 14:43:54 +0100

    Revert the last change. The sample for the matched inedx does not need to chaged since the route aae function take the sample rate into account.

--- source/navbench.py
+++ source/navbench.py
@@ -350,7 +350,6 @@ class Benchmark:
                     # Difference between matched index and minimum distance index and distance between points
                     matched_index = nav.get_index_log()
                     if matched_index:
-                        matched_index = np.asarray(matched_index) * ref_rep.sample_step
                         index_diffs = np.subtract(min_dist_index, nav.get_index_log())
                         dist_diff = calc_dists(ref_rep.get_xycoords(), min_dist_index, matched_index)
                         index_diffs = index_diffs.tolist()
