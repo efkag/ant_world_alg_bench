9997d9826cc4473964f87d01ae4b430680b7d17a
efkag <kagioulis.efstathios@gmail.com>
2024-08-02 09:59:46 +0100

    Intialise centroid to the max Blue value.

--- source/imageproc/imclust.py
+++ source/imageproc/imclust.py
@@ -12,11 +12,18 @@ def im_to_long_form(im: np.ndarray):
 
 
 def cluster_im(im: np.ndarray, whiten=False):
+    '''
+    im: Image array in BGR. Assumes the image array is in BRG format
+    because the cetroid for the sky is calculated based on the the
+    maximus blue value.
+    '''
     h, w, c = im.shape
     im_data = im_to_long_form(im)
     im_data = im_data.astype(np.float32, copy=False)
     if whiten: im_data = whiten(im_data)
-    centroids = [im_data[int(h/2)], im_data[int(-h/2)]]
+    # assumes BGR format!!
+    cent_sky = im_data[im_data[:, 0].argmax()]
+    centroids = [cent_sky, im_data[int(-h/2)]]
     centroids, labels = kmeans2(im_data, k=centroids)
     im_quant = labels.reshape(h, w)
     return im_quant
